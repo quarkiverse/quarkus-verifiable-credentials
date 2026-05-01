a5c8d6564631edb29d6377d3de367c34f902a2c8
Sergey Beryozkin <sberyozkin@gmail.com>
2026-05-01 16:16:13 +0100

    Initial structure

---
+++ verifier/deployment/src/main/java/io/quarkiverse/oidvp/deployment/OpenIdVerifiablePresentationsBuildStep.java
@@ -0,0 +1,59 @@
+package io.quarkiverse.oidvp.deployment;
+
+import java.util.function.BooleanSupplier;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.jboss.jandex.DotName;
+
+import io.quarkiverse.oidvp.CredentialIssuerMetadata;
+import io.quarkiverse.oidvp.runtime.OpenIdVerifiablePresentationsRecorder;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.Feature;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.BuildSteps;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
+import io.quarkus.proxy.deployment.ProxyRegistryBuildItem;
+import io.quarkus.tls.deployment.spi.TlsRegistryBuildItem;
+import io.quarkus.vertx.core.deployment.CoreVertxBuildItem;
+
+@BuildSteps(onlyIf = OpenIdVerifiablePresentationsBuildStep.IsEnabled.class)
+public class OpenIdVerifiablePresentationsBuildStep {
+
+    private static final DotName CREDENTIAL_ISSUER_METADATA = DotName
+            .createSimple(CredentialIssuerMetadata.class.getName());
+
+    @BuildStep
+    ExtensionSslNativeSupportBuildItem enableSslInNative() {
+        return new ExtensionSslNativeSupportBuildItem(Feature.OIDC);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    public void generateBean(
+            OpenIdVerifiablePresentationsRecorder recorder,
+            BuildProducer<SyntheticBeanBuildItem> beanProducer,
+            CoreVertxBuildItem vertxBuildItem,
+            TlsRegistryBuildItem tlsRegistryBuildItem,
+            ProxyRegistryBuildItem proxyRegistryBuildItem) {
+        beanProducer.produce(SyntheticBeanBuildItem
+                .configure(CREDENTIAL_ISSUER_METADATA)
+                .setRuntimeInit()
+                .defaultBean()
+                .scope(ApplicationScoped.class)
+                .supplier(recorder.setup(vertxBuildItem.getVertx(), tlsRegistryBuildItem.registry(),
+                        proxyRegistryBuildItem.registry()))
+                .done());
+    }
+
+    public static class IsEnabled implements BooleanSupplier {
+        OpenIdVerifiablePresentationsBuildTimeConfig config;
+
+        public boolean getAsBoolean() {
+            return config.enabled();
+        }
+    }
+}
